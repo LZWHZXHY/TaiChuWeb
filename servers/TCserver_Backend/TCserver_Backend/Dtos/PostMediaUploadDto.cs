39734ee3b8b6dfcd9096256cf89ce7593da09924
LZWHZXHY <LZWHZXHY@gmail.com>
2025-09-30 19:12:24 -0400

    停止跟踪 TCserver_Backend

--- servers/TCserver_Backend/TCserver_Backend/Dtos/PostMediaUploadDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TCserver_Backend.Dtos
-{
-    public class PostMediaUploadDto
-    {
-        public List<IFormFile> Images { get; set; }
-        public List<IFormFile> Videos { get; set; }
-    }
-}
